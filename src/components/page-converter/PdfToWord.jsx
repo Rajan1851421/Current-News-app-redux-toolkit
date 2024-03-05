0146649d6ed54295bd178239b10d8e7692dba609
Rajan Prajapati <rajanprajapati743@gmail.com>
2024-03-05 17:20:48 +0530

    ok

--- src/components/page-converter/PdfToWord.jsx
+++ src/components/page-converter/PdfToWord.jsx
@@ -22,7 +22,7 @@ function PdfToWord() {
             <div className='p-5  w-11/12 md:w-1/2 bg-red-400 shadow-lg rounded-md mt-[-150px]'>
                 <form action="/upload" method="post" encType="multipart/form-data" className="space-y-4 flex flex-col justify-center items-center mt-3">
 
-                    <h2 className="text-2xl font-bold mb-4">Select a PDF File</h2>
+                    <h2 className="text-2xl font-bold mb-4">Select a new PDF File</h2>
 
                     <input 
                             type="file" 
