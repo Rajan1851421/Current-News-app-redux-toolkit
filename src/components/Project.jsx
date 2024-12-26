624aaac1358d932e959a9753dccc7cc07f5065c6
Rajan Prajapati <rajanprajapati743@gmail.com>
2024-12-26 19:01:07 +0530

    project complete

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import youtube  from '../assets/image/youtube.png'
+import uttam from '../assets/image/uttam.png'
+import enote from '../assets/image/enote.png'
+import watch from '../assets/image/watch.png'
+import live from '../assets/image/live.png'
+import book from '../assets/image/book.png'
+import gyandeep from '../assets/image/gyandeep.png'
 
 function Project() {
     return (
@@ -14,11 +20,11 @@ function Project() {
                 <section class="overflow-hidden">
                     <div class="max-w-screen-xl 2xl:max-w-screen-3xl px-8 md:px-12 mx-auto py-20 lg:py-24 space-y-24 h-svh flex flex-col justify-center">
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 mx-auto space-y-3">
-                            <a href="https://uttamtravels.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        <a href="https://uttamtravels.netlify.app/" target="_blank" rel="noopener noreferrer">
                                 <img
-                                    src="https://res-console.cloudinary.com/ds8ttmj8w/thumbnails/v1/video/upload/v1732598254/QmxhY2tfWWVsbG93X01vZGVybl9Qcm9kdWN0X0luc3RhZ3JhbV9Qb3N0X3U1M2J0ZA==/drilldown"
-                                    className="rounded-xl rotate-6 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-48 object-cover hover:scale-105 transform origin-bottom"
-                                    alt="Project 1"
+                                    src={uttam}
+                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-auto object-cover hover:scale-105 transform origin-bottom"
+                                    alt="Project 2"
                                 />
                             </a>
                             <a href="https://rajantube.netlify.app/" target="_blank" rel="noopener noreferrer">
@@ -28,45 +34,39 @@ function Project() {
                                     alt="Project 2"
                                 />
                             </a>
-                            <a href="#_">
-                                <img
-                                    src="https://res-console.cloudinary.com/ds8ttmj8w/thumbnails/v1/image/upload/v1732598814/Q2FwdHVyZV9kanBiYjk=/drilldown"
-                                    className="rounded-xl rotate-6 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-48 object-cover hover:scale-105 transform origin-bottom"
-                                    alt="Project 3"
-                                />
-                            </a>
+                           
                             <a href="https://eenote.netlify.app/" target="_blank" rel="noopener noreferrer">
                                 <img
-                                    src="https://res-console.cloudinary.com/ds8ttmj8w/thumbnails/v1/image/upload/v1732598812/Q2FwdHVyZV80X21tdzVqZQ==/drilldown"
-                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-48 object-cover hover:scale-105 transform origin-bottom"
+                                    src={enote}
+                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-auto object-cover hover:scale-105 transform origin-bottom"
                                     alt="Project 4"
                                 />
                             </a>
                             <a href="https://graceful-pastelito-01b3d7.netlify.app/" target="_blank" rel="noopener noreferrer">
                                 <img
-                                    src="https://res-console.cloudinary.com/ds8ttmj8w/thumbnails/v1/image/upload/v1732598812/Q2FwdHVyZV8zX2VkYnp2MA==/drilldown"
-                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-48 object-cover hover:scale-105 transform origin-bottom"
+                                    src={watch}
+                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-auto object-cover hover:scale-105 transform origin-bottom"
                                     alt="Project 5"
                                 />
                             </a>
                             <a href="https://liveclgstream.netlify.app/" target="_blank" rel="noopener noreferrer">
                                 <img
-                                    src="https://res-console.cloudinary.com/ds8ttmj8w/thumbnails/v1/image/upload/v1732598812/Q2FwdHVyZV81X2ZiY2pvMA==/drilldown"
-                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-48 object-cover hover:scale-105 transform origin-bottom"
+                                    src={live}
+                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-auto object-cover hover:scale-105 transform origin-bottom"
                                     alt="Project 6"
                                 />
                             </a>
                             <a href="http://gdpmcvns.com/" target="_blank" rel="noopener noreferrer">
                                 <img
-                                    src="https://res.cloudinary.com/ds8ttmj8w/image/upload/v1732599702/Capture_6_dzhiau.png"
-                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-48 object-cover hover:scale-105 transform origin-bottom"
+                                    src={gyandeep}
+                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-auto object-cover hover:scale-105 transform origin-bottom"
                                     alt="Project 7"
                                 />
                             </a>
                             <a href="https://bookbuyme.netlify.app/" target="_blank" rel="noopener noreferrer">
                                 <img
-                                    src="https://res.cloudinary.com/ds8ttmj8w/image/upload/v1732599700/7_egoqdw.png"
-                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-48 object-cover hover:scale-105 transform origin-bottom"
+                                    src={book}
+                                    className="rounded-xl -rotate-12 hover:rotate-0 duration-500 hover:-translate-y-12 w-full h-auto object-cover hover:scale-105 transform origin-bottom"
                                     alt="Project 8"
                                 />
                             </a>
